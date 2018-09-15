b3f2bcabf91b4d8a2b42a2848a7f06dce38b99ba
Antonio Brewer <yodabox64@hotmail.com>
2018-09-15 17:42:17 -0400

    Add files via upload

---
+++ examples/CANquittoNode112/CANquittoNode112.ino
@@ -0,0 +1,35 @@
+#include <IFCT.h>
+#include <CANquitto.h>
+
+void setup() {
+  pinMode(2, OUTPUT); // for the transceiver enable pin
+  Can0.setBaudRate(1000000);
+  Can0.enableFIFO();
+  Can0.enableFIFOInterrupt();
+  Node.onReceive(myCallback);
+  Node.begin(112);
+  Can0.intervalTimer(); // enable queue system and run callback in background.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    uint8_t buf[200];
+    for ( uint32_t i = 0; i < sizeof(buf); i++ ) buf[i] = random(0, 255); // i + 1;
+    if (Node.write(buf, sizeof(buf), 123, 2000, 3000)) digitalWrite(13, !digitalRead(13));
+    delay(100);
+}
+
+void myCallback(const uint8_t* buffer, uint16_t length, AsyncCQ info) {
+  Serial.print("PAYLOAD!!: ");
+  for ( uint32_t i = 0; i < length; i++ ) {
+    Serial.print((char)buffer[i]);
+  } Serial.println();
+  Serial.print("From Node: ");
+  Serial.println(info.node);
+
+  Serial.print("Length: ");
+  Serial.println(length);
+
+  Serial.print("Millis(): ");
+  Serial.println(millis());
+}
---
+++ examples/CANquittoNode123/CANquittoNode123.ino
@@ -0,0 +1,34 @@
+#include <IFCT.h>
+#include <CANquitto.h>
+
+void setup() {
+  pinMode(2, OUTPUT); // for the transceiver enable pin
+  Can0.setBaudRate(1000000);
+  Can0.enableFIFO();
+  Can0.enableFIFOInterrupt();
+  Node.onReceive(myCallback);
+  Node.begin(123);
+  Can0.intervalTimer(); // enable queue system and run callback in background.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char buf[] = "Hello World! Hello Teensy! Hello IFCT!";
+  if (Node.write((const uint8_t*)buf, sizeof(buf), 112, 11, 2000, 3000)) digitalWrite(13, !digitalRead(13));
+  delay(100);
+}
+
+void myCallback(const uint8_t* buffer, uint16_t length, AsyncCQ info) {
+  Serial.print("PAYLOAD!!: ");
+  for ( uint32_t i = 0; i < length; i++ ) {
+    Serial.print(buffer[i]); Serial.print(" ");
+  } Serial.println();
+  Serial.print("From Node: ");
+  Serial.println(info.node);
+
+  Serial.print("Length: ");
+  Serial.println(length);
+
+  Serial.print("Millis(): ");
+  Serial.println(millis());
+}
